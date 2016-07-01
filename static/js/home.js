eb13b8f414e71f024da9665dd496a1feffddef8b
brnabus <bybrny@hotmail.co.nz>
2016-07-02 09:13:55 +1200

    Pull this shit on both work often

--- static/js/home.js
+++ static/js/home.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 
     function myMessage(){
         $('#story').typed({
-            strings: ["Hello."],
+            strings: ["Hello.", "There is something stopping humanity from being totally equal.","I believe the best future for us can be achieved by trying our hardest collectively to minimize the two biggest problems we face in our society.","Greed.","and","Scarcity.","The smaller these problems become, the more equal we will become collectively.","How can we make these problems smaller?"],
             backDelay: 500,
             typeSpeed: 30,
             backSpeed: 30,
@@ -20,6 +20,25 @@ $(document).ready(function() {
     function startMessage(){
         $('.content').animate({opacity:"+1"}, 2000);
         setTimeout(myMessage, 3000);
+	setTimeout(inputPlease, 55000);
+    };
+
+    function inputPlease(){
+	var x = document.createElement("form");
+        x.setAttribute('method',"post");
+        x.setAttribute('action',"/");
+        x.setAttribute('id', "formsend");
+
+        var y = document.createElement("input");
+        y.setAttribute('type', "text");
+        y.setAttribute('placeholder', "any input");
+        y.setAttribute('name', "input");
+        y.style.background = "black";
+        y.style.color = "white";
+        y.style.border = "0";
+	y.style.textAlign = "center";
+        x.appendChild(y);
+        document.getElementsByTagName('body')[0].appendChild(x);
     };
 
     setTimeout(startMessage, 3000);
