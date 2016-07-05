c9cf56974f25f33d7eba5ae34d20093f56149c2b
brnabus <bybrny@hotmail.co.nz>
2016-07-06 05:18:11 +1200

    lel

---
+++ static/js/plan.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('#story').typed({
+		strings: ["Hello."],
+		backDelay: 500,
+	        typeSpeed: 30,
+                backSpeed: 30,
+                startDelay: 1500,
+                loop: false,
+                loopCount: false,
+                showCursor: false
+	});
+});
