df5e5dec9ce1122590916a476ca77374a1657c54
Andre Cowie <drecowie@gmail.com>
2017-04-26 20:28:59 +1200

    Graphs up

--- graphs/__init__.py
+++ graphs/__init__.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 import os
 from datetime import datetime
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
+    logger.info(str(request.environ['REMOTE_ADDR']))
     return render_template('index.html')
 
 app.secret_key = 'anotherBigSecretGuessthis12456939433517254'
