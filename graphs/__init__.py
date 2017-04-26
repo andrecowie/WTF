7b32f37be288e7cef29d97c39ce0c282d6b41f47
Andre Cowie <drecowie@gmail.com>
2017-04-26 12:59:47 +1200

    Some work on making a graphs page

---
+++ graphs/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, url_for
+import os
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+app.secret_key = 'anotherBigSecretGuessthis12456939433517254'
+
+if __name__ == '__main__':
+    app.run()
+
